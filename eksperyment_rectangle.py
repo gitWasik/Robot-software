f796857cb81c07ef2650cf9f272e13fa57958602
gitWasik <110221000+gitWasik@users.noreply.github.com>
2024-02-19 17:02:20 +0100

    Update eksperyment_rectangle.py

--- eksperyment_rectangle.py
+++ eksperyment_rectangle.py
@@ -29,7 +29,7 @@ def Hand_Open(hand_landmarks):
 def Capture_Video(): 
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(static_image_mode = False, max_num_hands = 1,
-                           min_detection_confidence = 0.5, 
+                           min_detection_confidence = 0.7, 
                            min_tracking_confidence = 0.5)
     cam = cv2.VideoCapture(0)
     if not cam.isOpened():
